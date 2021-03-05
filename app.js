13f16fbb5e96a79e71ad863a86f1a2322662a7c9
umamah1mahmoud <umamah.m.94@gmail.com>
2021-03-05 16:46:27 +0200

    problem 1 string update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
     let sum=a+b;
-    let array1= [sum, 'The sum of' + a + 'and' + b + 'is' + sum + '.'];
+    let array1= [sum, 'The sum of ' + a + ' and ' + b + ' is ' + sum + '.'];
     //console.log(array1);
     return array1;
 
