3bb991d7101326b724ca1f5f165410cfd62eef96
umamah1mahmoud <umamah.m.94@gmail.com>
2021-03-05 13:45:18 +0200

    added p3

--- app.js
+++ app.js
@@ -56,12 +56,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    let sum= a+b+c;
+    let multiply= a*b*c;
+    let array3=[sum, multiply, a + 'and' + b + 'and' + c + 'sum to' + sum + '.', 'The product of' + a + 'and' + b + 'and' + c + 'is' + multiply + '.'];
+    console.log();
+    return array3;
     
-
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
